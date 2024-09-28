45d236b1857dddc9cbfdd6090a84e5ac1d2dfad1
slyeet03 <shobhit7419@gmail.com>
2024-09-28 12:51:22 +0530

    Update spoify.rb

--- Formula/spoify.rb
+++ Formula/spoify.rb
@@ -1,9 +1,9 @@
 class Spoify < Formula
   desc "Spotify Player right in your terminal"
   homepage "https://github.com/slyeet03/spoify"
-  url "https://github.com/slyeet03/spoify/releases/download/v0.2.9/spoify-mac.tar.gz"
-  sha256 "6e0a629dbcb469473d95eec3d7dee0f44633ffc407a2579a1cddb1585358dfbc"
-  version "0.2.9"
+  url "https://github.com/slyeet03/spoify/releases/download/v0.2.10/spoify-mac.tar.gz"
+  sha256 "7da417ab90809a7870ad370595319b6f1a8593f11e9ccb223ce19ef3a9cd5b34"
+  version "0.2.10"
 
   def install
     bin.install "spoify"
